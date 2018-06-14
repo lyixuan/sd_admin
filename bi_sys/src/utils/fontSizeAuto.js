d2cd65f645f83fd9c60afbd0b72e05b033c77b3b
wangjing <1915093132@qq.com>
2018-06-14 10:20:16 +0800

    delete

--- bi_sys/src/utils/fontSizeAuto.js
+++
@@ -1,7 +0,0 @@
-export function fontSizeAuto(fontSize = null) {
-  //根据root节点的宽度进行动态设置字体的样式
-  let root_width = parseFloat(document.documentElement.style.fontSize);
-  !fontSize && console.log('传递参数');
-  let nodeFontsize=root_width * parseFloat(fontSize) / 100;
-  return nodeFontsize.toFixed(2);
-}
--- bi_sys/src/utils/typeDict.js
+++
@@ -1,2 +0,0 @@
-const familyTypeDict = {selfExam: 0, barrier: 1, incubator: 2};
-export {familyTypeDict};
