abe4432101224fd8e1edb7885054e307a2824794
wangjing <1915093132@qq.com>
2018-06-14 18:37:36 +0800

    修改

--- admin_sys/src/routes/Account/AccountList.js
+++ admin_sys/src/routes/Account/AccountList.js
@@ -33,26 +33,40 @@ class AccountList extends Component {
         render: (text, record) => {
           return this.state.dataSource.length > 1 ? (
             <Popconfirm title="Sure to delete?" onConfirm={() => this.onDelete(record.key)}>
-              编辑
+              <a href="">编辑</a>
             </Popconfirm>
           ) : null;
         },
       },
     ];
 
-    const dataSource = [];
-    for (let i = 0; i < 3; i + 1) {
-      dataSource.push({
-        key: i,
-        name: `张三 ${i + 1}`,
-        role: `院长${i + 1}`,
-        status: `启用${i + 1}`,
-        email: `hello${i + 1}@sunlands.com`,
-      });
-    }
+    const dataSource = [
+      {
+        key: 1,
+        name: `张三`,
+        role: `院长`,
+        status: `启用`,
+        email: `hello@sunlands.com`,
+      },
+      {
+        key: 2,
+        name: `王五`,
+        role: `学员`,
+        status: `启用`,
+        email: `hello@sunlands.com`,
+      },
+      {
+        key: 3,
+        name: `赵六`,
+        role: `院长`,
+        status: `禁止`,
+        email: `hello@sunlands.com`,
+      },
+    ];
+
     this.state = {
-      dataSource: !dataSource ? dataSource : [],
-      count: 0,
+      dataSource: !dataSource ? dataSource : dataSource,
+      count: 3,
     };
   }
   onDelete = key => {
@@ -75,7 +89,7 @@ class AccountList extends Component {
   };
   render() {
     const { dataSource } = this.state;
-    const columns = !this.columns ? this.columns : [];
+    const columns = !this.columns ? this.columns : this.columns;
     return (
       <PageHeaderLayout>
         <Button onClick={this.handleAdd} type="primary" style={{ marginBottom: 16 }}>
