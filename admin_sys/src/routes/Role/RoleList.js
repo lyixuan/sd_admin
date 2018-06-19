1d9048309f5d4b41619c52790ddbe382ae74e291
lixioajing <lixiaojing01@sunlands.com>
2018-06-19 17:22:07 +0800

    角色表单demo

--- admin_sys/src/routes/Role/RoleList.js
+++ admin_sys/src/routes/Role/RoleList.js
@@ -1,27 +1,205 @@
 import React, { Component } from 'react';
-import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+import { Table, Button, Popconfirm, Pagination, Form, Row, Col, Input } from 'antd';
+
+import ContentLayout from '../../layouts/ContentLayout';
+import styles from '../../common/AdvancedSearchForm.css';
+
+const params = {
+  username: '用户名',
+  password: '密码',
+};
+const FormItem = Form.Item;
+let propsVal = '';
+
 class RoleList extends Component {
   constructor(props) {
     super(props);
+    this.columns = [
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        width: '100px',
+      },
+      {
+        title: '角色',
+        dataIndex: 'role',
+        width: '200px',
+      },
+      {
+        title: '邮箱',
+        dataIndex: 'email',
+        width: '300px',
+      },
+      {
+        title: '状态',
+        dataIndex: 'status',
+        width: '100px',
+      },
+      {
+        title: '操作',
+        dataIndex: 'operation',
+        width: '100px',
+        render: (text, record) => {
+          return this.state.dataSource.length > 1 ? (
+            <Popconfirm title="Sure to delete?" onConfirm={() => this.onDelete(record.key)}>
+              <a href="">编辑</a>
+            </Popconfirm>
+          ) : null;
+        },
+      },
+    ];
+
+    const dataSource = [
+      {
+        key: 1,
+        name: `张三`,
+        role: `院长`,
+        status: `启用`,
+        email: `hello@sunlands.com`,
+      },
+      {
+        key: 2,
+        name: `王五`,
+        role: `学员`,
+        status: `启用`,
+        email: `hello@sunlands.com`,
+      },
+      {
+        key: 3,
+        name: `赵六`,
+        role: `院长`,
+        status: `禁止`,
+        email: `hello@sunlands.com`,
+      },
+    ];
+
     this.state = {
-    }
+      dataSource: !dataSource ? [] : dataSource,
+      count: 3,
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.props.getUrlParams());
+    // const param = this.props.getUrlParams();
+    // propsVal.form.setFieldsValue({
+    //   username: 2131,
+    //   password: `${param.password?param.password:''}`,
+    // });
+  }
+
+  onDelete = key => {
+    const dataSource = [...this.state.dataSource];
+    this.setState({ dataSource: dataSource.filter(item => item.key !== key) });
   };
-  componentDidMount(){
-    // this.props.setUrlParams()
-  }
-  onClick=()=>{
-    this.props.setCurrentUrlParams({s:5})
-  }
+
+  onShowSizeChange = (current, pageSize) => {
+    console.log(current, pageSize);
+  };
+  getFields = () => {
+    const count = 4;
+    const { getFieldDecorator } = propsVal.form;
+    const children = [];
+    Object.keys(params).map((key, i) => {
+      return children.push(
+        <Col span={8} key={key} style={{ display: i < count ? 'block' : 'none' }}>
+          <FormItem label={params[key]}>
+            {getFieldDecorator(key, {
+              rules: [
+                {
+                  required: true,
+                  message: '请输入搜索内容!',
+                },
+              ],
+            })(<Input placeholder={`请输入${params[key]}`} />)}
+          </FormItem>
+        </Col>
+      );
+    });
+    return children;
+  };
+  handleSearch = e => {
+    e.preventDefault();
+    let val = {};
+    propsVal.form.validateFields((err, values) => {
+      val = values;
+    });
+    this.props.setCurrentUrlParams(val);
+    propsVal.form.setFieldsValue({
+      username: 2131,
+      password: 222,
+    });
+  };
+
+  handleReset = () => {
+    propsVal.form.resetFields();
+    this.props.setCurrentUrlParams({ s: 5 });
+  };
+  handleAdd = () => {
+    const { count, dataSource } = this.state;
+    this.props.history.push({
+      pathname: '/role/roleList',
+      search: JSON.stringify({ s: 2 }),
+    });
+    const newData = {
+      key: count + 1,
+      name: `李四 ${count + 1}`,
+      role: `质检员${count + 1}`,
+      status: `禁止${count + 1}`,
+      email: `world${count + 1}@sunlands.com`,
+    };
+    this.setState({
+      dataSource: [...dataSource, newData],
+      count: count + 1,
+    });
+  };
   render() {
-   console.log(this)
+    const { dataSource } = this.state;
+    const columns = !this.columns ? [] : this.columns;
+
+    const WrappedAdvancedSearchForm = Form.create()(props => {
+      propsVal = props;
+      return (
+        <Form className={styles.searchForm} onSubmit={this.handleSearch}>
+          <Row gutter={24}>{this.getFields()}</Row>
+          <Row>
+            <Col span={24} style={{ textAlign: 'right' }}>
+              <Button type="primary" htmlType="submit">
+                搜索
+              </Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
+                重置
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      );
+    });
     return (
-      <PageHeaderLayout>
-        <div onClick={this.onClick}>
-          角色列表
-        </div>
-       </PageHeaderLayout>
+      <ContentLayout
+        contentForm={<WrappedAdvancedSearchForm />}
+        contentButton={
+          <Button
+            onClick={this.handleAdd}
+            type="primary"
+            style={{ marginBottom: 16, marginTop: 20 }}
+          >
+            + 创建
+          </Button>
+        }
+        contentTable={
+          <Table bordered dataSource={dataSource} columns={columns} pagination={false} />
+        }
+        contentPagination={
+          <Pagination
+            showSizeChanger
+            onShowSizeChange={this.onShowSizeChange}
+            defaultCurrent={3}
+            total={100}
+          />
+        }
+      />
     );
   }
 }
-
 export default RoleList;
