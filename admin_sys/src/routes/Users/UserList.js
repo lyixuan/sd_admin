a84202a39cecab646141290ae3a08c72b2a40d33
wangjing <1915093132@qq.com>
2018-06-20 22:00:31 +0800

    用户布局

--- admin_sys/src/routes/Users/UserList.js
+++ admin_sys/src/routes/Users/UserList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Table, Button } from 'antd';
+import { Table, Button, Form, Input } from 'antd';
 import ContentLayout from '../../layouts/ContentLayout';
 import AuthorizedButton from '../../selfComponent/AuthorizedButton';
 import styles from '../Account/Account.css';
 import common from '../Common/common.css';
 
+const FormItem = Form.Item;
 class UserList extends Component {
   constructor(props) {
     super(props);
@@ -84,6 +85,7 @@ class UserList extends Component {
 
     this.state = {
       dataSource: !dataSource ? [] : dataSource,
+      formLayout: 'inline',
     };
   }
   onEdit = () => {
@@ -103,8 +105,32 @@ class UserList extends Component {
   render() {
     const { dataSource } = this.state;
     const columns = !this.columns ? [] : this.columns;
+    const { formLayout } = this.state;
+    const formItemLayout = null;
+    const buttonItemLayout = null;
+    const WrappedAdvancedSearchForm = Form.create()(() => {
+      return (
+        <Form layout={formLayout}>
+          <FormItem label="姓名" {...formItemLayout}>
+            <Input placeholder="q请输入" />
+          </FormItem>
+          <FormItem label="手机" {...formItemLayout}>
+            <Input placeholder="请输入" />
+          </FormItem>
+          <FormItem {...buttonItemLayout}>
+            <Button type="primary" className={common.createButton}>
+              搜索
+            </Button>
+            <Button type="primary" className={styles.cancleButton}>
+              重置
+            </Button>
+          </FormItem>
+        </Form>
+      );
+    });
     return (
       <ContentLayout
+        contentForm={<WrappedAdvancedSearchForm />}
         contentButton={
           <AuthorizedButton authority="/user/createUser">
             <Button onClick={this.handleAdd} type="primary" className={common.createButton}>
