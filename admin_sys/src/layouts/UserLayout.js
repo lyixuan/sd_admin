3177bdd358b6487084efdcc74253291ac10be4cb
mzh <manzhenhua@sunlands.com>
2018-06-21 18:18:57 +0800

    add

--- admin_sys/src/layouts/UserLayout.js
+++ admin_sys/src/layouts/UserLayout.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Link, Redirect, Switch, Route } from 'dva/router';
+import { Redirect, Switch, Route } from 'dva/router';
 import DocumentTitle from 'react-document-title';
 import styles from './UserLayout.less';
+import logo from '../assets/logo.png';
+import peopleImg from '../assets/bj.png';
 import { getRoutes } from '../utils/utils';
 
 class UserLayout extends React.PureComponent {
@@ -14,30 +16,39 @@ class UserLayout extends React.PureComponent {
     }
     return title;
   }
+
   render() {
     const { routerData, match } = this.props;
     return (
       <DocumentTitle title={this.getPageTitle()}>
         <div className={styles.container}>
           <div className={styles.content}>
-            <div className={styles.top}>
-              <div className={styles.header}>
-                <Link to="/">
-                  <span className={styles.title}>尚德机构管理系统</span>
-                </Link>
+            <div className={styles.background} />
+            <div className={styles.showContent}>
+              <img src={peopleImg} alt="people" className={styles.peopleImg} />
+
+              <div className={styles.loginContainer}>
+                <div className={styles.loginContent}>
+                  <span className={styles.header}>
+                    <img src={logo} alt="logo" />
+                    <h3>小德-后台管理系统</h3>
+                  </span>
+                  <div className={styles.login}>
+                    <Switch>
+                      {getRoutes(match.path, routerData).map(item => (
+                        <Route
+                          key={item.key}
+                          path={item.path}
+                          component={item.component}
+                          exact={item.exact}
+                        />
+                      ))}
+                      <Redirect exact from="/user" to="/userLayout/login" />
+                    </Switch>
+                  </div>
+                </div>
               </div>
             </div>
-            <Switch>
-              {getRoutes(match.path, routerData).map(item => (
-                <Route
-                  key={item.key}
-                  path={item.path}
-                  component={item.component}
-                  exact={item.exact}
-                />
-              ))}
-              <Redirect exact from="/user" to="/userLayout/login" />
-            </Switch>
           </div>
         </div>
       </DocumentTitle>
--- admin_sys/src/utils/checkoutUserAuthInfo.js
+++ admin_sys/src/utils/checkoutUserAuthInfo.js
@@ -1,9 +1,12 @@
-import { getAuthority } from './authority';
-export function checkoutLogin(){
-  const userInfo=JSON.parse(getAuthority('userInfo'));
-  return userInfo!==null;
+// import { getAuthority } from './authority';
+
+export function checkoutLogin() {
+  // const userInfo = JSON.parse(getAuthority('userInfo'));
+  // return userInfo!==null;
+  return false;
 }
-export function checkoutAuthRoute(pathName){
-  const authInfo=JSON.parse(getAuthority('authInfo'));
-  return true
+export function checkoutAuthRoute(pathName) {
+  console.log(pathName);
+  // const authInfo = JSON.parse(getAuthority('authInfo'));
+  return true;
 }
