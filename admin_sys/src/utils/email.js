e87f3fdb20526c0693d68afd4f1db326d3fe1d27
wangjing <1915093132@qq.com>
2018-07-02 20:01:20 +0800

    完善email校验逻辑

--- admin_sys/src/utils/email.js
+++ admin_sys/src/utils/email.js
@@ -1,4 +1,9 @@
 export function formatEmail(str) {
-  const changeEmail = !str.substring(0, str.indexOf('@'))?str:str.substring(0, str.indexOf('@'));
-  return changeEmail;
+  if(str && typeof str==='string'){
+    const changeEmail = !str.substring(0, str.indexOf('@'))?str:str.substring(0, str.indexOf('@'));
+    return changeEmail;
+  }else{
+    console.error("传入非字符串数据")
+  }
+
 }
