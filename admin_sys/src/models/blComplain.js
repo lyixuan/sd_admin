34c5a7b4bb16ec8b72cffbe67a0b65df36f51a41
lixioajing <lixiaojing01@sunlands.com>
2018-07-06 18:25:40 +0800

    底线投诉api

--- admin_sys/src/models/blComplain.js
+++ admin_sys/src/models/blComplain.js
@@ -1,104 +1,42 @@
-import { routerRedux } from 'dva/router';
 import { message } from 'antd';
-import {
-  queryAccountList,
-  addAccount,
-  updateAccount,
-  deleteAccount,
-  getRoleList,
-  queryAccountInfo,
-} from '../../services/api';
+import { getBlComplainList, preDelBlComplainList, delBlComplainList } from '../services/api';
 
 export default {
   namespace: 'blComplain',
 
-  state: {
-    // 请求接口上送参数
-    accountListParams: { size: 30, number: 0 },
-    addAccountParams: {},
-    updateAccountParams: {},
-    deleteAccountParams: {},
-    getRoleListParams: {},
-    accountInfoParams: {},
-    // 接口返回数据存储
-    accountList: [],
-    getRoleList: [],
-    accountInfo: [],
-  },
+  state: {},
 
   effects: {
-    *accountList({ payload }, { call, put }) {
+    *getList({ payload }, { call, put }) {
       const { accountListParams } = payload;
-      const response = yield call(queryAccountList, { ...accountListParams });
+      const response = yield call(getBlComplainList, { ...accountListParams });
       console.log(response);
-      yield put({ type: 'accountListSave', payload: { response } });
+      yield put({ type: 'save', payload: { response } });
     },
-    *accountInfo({ payload }, { call, put }) {
-      console.log('进入info的接口');
-      const { accountInfoParams } = payload;
-      const response = yield call(queryAccountInfo, { ...accountInfoParams });
-      console.log(response);
-      yield put({ type: 'accountInfoSave', payload: { response } });
-    },
-    *addAccount({ payload }, { call, put }) {
-      const result = yield call(addAccount, payload.addAccountParams);
-      if (result.code === 0 || result.code === 2000) {
-        message.success('账号添加成功！');
-        yield put(routerRedux.push('/account/accountList'));
-      } else {
-        message.error(result.msg);
-      }
-    },
-    *updateAccount({ payload }, { call, put }) {
-      const result = yield call(updateAccount, payload.updateAccountParams);
+    *perDelComplain({ payload }, { call }) {
+      const result = yield call(preDelBlComplainList, payload.updateAccountParams);
       if (result.code === 0 || result.code === 2000) {
         message.success('账号编辑成功！');
-        yield put(routerRedux.push('/account/accountList'));
       } else {
         message.error(result.msg);
       }
     },
-    *deleteAccount({ payload }, { call, put }) {
+    *delComplain({ payload }, { call }) {
       console.log(payload.deleteAccountParams);
-      const result = yield call(deleteAccount, payload.deleteAccountParams);
+      const result = yield call(delBlComplainList, payload.deleteAccountParams);
       if (result.code === 0 || result.code === 2000) {
         message.success('账号删除成功！');
       } else {
         message.error(result.msg);
       }
-      const response = yield call(queryAccountList, { size: 30, number: 0 });
-      console.log(response);
-      yield put({
-        type: 'accountListSave',
-        payload: { response },
-      });
-    },
-    *getRoleList({ payload }, { call, put }) {
-      const response = yield call(getRoleList, payload.getRoleListParams);
-      yield put({
-        type: 'getRoleListSave',
-        payload: response,
-      });
     },
   },
 
   reducers: {
-    accountListSave(state, action) {
+    save(state, action) {
       return {
         ...state,
-        accountList: action.payload,
-      };
-    },
-    accountInfoSave(state, action) {
-      return {
-        ...state,
-        accountInfo: action.payload,
-      };
-    },
-    getRoleListSave(state, action) {
-      return {
-        ...state,
-        getRoleList: action.payload,
+        ...action.payload,
       };
     },
   },
