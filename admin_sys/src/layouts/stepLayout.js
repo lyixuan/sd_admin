bd396df071a9a385221a830ce910264879cbfc1a
lixioajing <lixiaojing01@sunlands.com>
2018-07-11 16:55:08 +0800

    修改上传地址

--- admin_sys/src/layouts/stepLayout.js
+++ admin_sys/src/layouts/stepLayout.js
@@ -6,8 +6,8 @@
 * step1Fetch, 第一步接口请求
 * step2Fetch, 第二步接口请求
 * step3Fetch, 第三步接口请求
-* nums,       所有的nums
-* successNums 校验成功的nums
+* current,    当前步骤下标
+* editCurrent 修改步骤
 * */
 import React, { Component } from 'react';
 import { Steps, Button } from 'antd';
@@ -17,31 +17,22 @@ import common from '../routes/Common/common.css';
 const { Step } = Steps;
 
 class StepLayout extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      current: 0,
-    };
-  }
-
   // 下一页
   next() {
-    const current = this.state.current + 1;
-    const { step1Fetch, step2Fetch, step3Fetch } = this.props;
-    this.setState({ current });
-    if (current === 1) {
+    const { step1Fetch, step2Fetch, step3Fetch, current } = this.props;
+    if (current === 0) {
       if (step1Fetch) {
         step1Fetch();
       } else {
         console.warn('添加step1Fetch方法');
       }
-    } else if (current === 2) {
+    } else if (current === 1) {
       if (step2Fetch) {
         step2Fetch();
       } else {
         console.warn('添加step2Fetch方法');
       }
-    } else if (current === 3) {
+    } else if (current === 2) {
       if (step3Fetch) {
         step3Fetch();
       } else {
@@ -53,8 +44,8 @@ class StepLayout extends Component {
   }
   // 上一页
   prev() {
-    const current = this.state.current - 1;
-    this.setState({ current });
+    const { editCurrent, current } = this.props;
+    editCurrent(current - 1);
   }
   // 取消---回到列表页
   cancel = () => {
@@ -65,8 +56,7 @@ class StepLayout extends Component {
     this.props.goBack();
   };
   render() {
-    const { title, steps, baseLayout, isDisabled } = this.props;
-    const { current } = this.state;
+    const { title, steps, baseLayout, isDisabled, current } = this.props;
 
     const stepBlock = (
       <div>
@@ -75,20 +65,20 @@ class StepLayout extends Component {
             <Steps current={current} progressDot>
               {steps.map(item => <Step key={item.title} title={item.title} />)}
             </Steps>
-            <div>{steps[this.state.current].content}</div>
+            <div>{steps[current].content}</div>
             <div className={styles.stepsAction}>
-              {this.state.current === 0 && (
+              {current === 0 && (
                 <Button onClick={() => this.cancel()} className={common.cancleButton}>
                   取消
                 </Button>
               )}
-              {this.state.current > 0 &&
-                this.state.current !== steps.length - 1 && (
+              {current > 0 &&
+                current !== steps.length - 1 && (
                   <Button onClick={() => this.prev()} className={common.cancleButton}>
                     上一步
                   </Button>
                 )}
-              {this.state.current < steps.length - 1 && (
+              {current < steps.length - 1 && (
                 <Button
                   className={common.submitButton}
                   type="primary"
@@ -98,7 +88,7 @@ class StepLayout extends Component {
                   下一步
                 </Button>
               )}
-              {this.state.current === steps.length - 1 && (
+              {current === steps.length - 1 && (
                 <Button type="primary" onClick={this.clickOk} className={common.submitButton}>
                   确定
                 </Button>
