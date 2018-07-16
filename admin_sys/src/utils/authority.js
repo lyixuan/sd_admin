8d8d97436eb233149b84b8618e6b3c5598636e5e
mzh <manzhenhua@sunlands.com>
2018-07-16 18:59:06 +0800

    修改用户设置不允许本地存储时的提示

--- admin_sys/src/utils/authority.js
+++ admin_sys/src/utils/authority.js
@@ -1,17 +1,33 @@
 // use localStorage to store the authority info, which might be sent from server in actual project.
 export function getAuthority(key) {
-  const value = localStorage.getItem(key) || sessionStorage.getItem(key);
-  return value ? JSON.parse(value) : value;
+  try {
+    const value = localStorage.getItem(key) || sessionStorage.getItem(key);
+    return value ? JSON.parse(value) : value;
+  } catch (e) {
+    alert('读取本地存储失败,请检查浏览器对存储的设置');
+  }
 }
 
 export function setAuthority(key, value) {
-  return localStorage.setItem(key, JSON.stringify(value));
+  try {
+    return localStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    alert('读取本地存储失败,请检查浏览器对存储的设置');
+  }
 }
 export function removeStorge(key) {
-  window.localStorage.removeItem(key);
-  window.sessionStorage.removeItem(key);
+  try {
+    window.localStorage.removeItem(key);
+    window.sessionStorage.removeItem(key);
+  } catch (e) {
+    alert('读取本地存储失败,请检查浏览器对存储的设置');
+  }
 }
 // 使用seccion会话记录不进行免登陆账号的问题
 export function setAuthoritySeccion(key, value) {
-  return window.sessionStorage.setItem(key, JSON.stringify(value));
+  try {
+    return window.sessionStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    alert('读取本地存储失败,请检查浏览器对存储的设置');
+  }
 }
