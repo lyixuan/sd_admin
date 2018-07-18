a7d8487403aa327118d60d11dd5a8aa93dc54431
wangjing <1915093132@qq.com>
2018-07-18 19:15:52 +0800

    添加所有页面功能配置

--- admin_sys/src/common/urlPathMap.js
+++ admin_sys/src/common/urlPathMap.js
@@ -14,13 +14,25 @@ const urlPathMap = {
     name: '用户删除',
   },
   '/complaintDoubles/editeComplaintDoubles': {
-    name: '编辑投诉倍数',
+    name: '编辑扣分倍数',
   },
   '/timeManage/unAddDate': {
     name: '不可选日期',
   },
+  '/timeManage/deleteDate': {
+    name: '删除不可用日期',
+  },
   '/user/updateUser': {
     name: '更新用户',
+  },
+  '/group/editeGroupShortName': {
+    name: '编辑小组短名称',
+  },
+  '/college/editeCollegeShortName': {
+    name: '编辑学院短名称',
+  },
+  '/family/editeFamilyShortName': {
+    name: '编辑家族短名称',
   },
 };
 export { urlPathMap };
