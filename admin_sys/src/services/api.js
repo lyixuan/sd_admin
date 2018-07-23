4d45f4d2451920acc0fbd37a0504ca60fa640c0c
mzh <manzhenhua@sunlands.com>
2018-07-23 19:12:36 +0800

    修改api

--- admin_sys/src/services/api.js
+++ admin_sys/src/services/api.js
@@ -5,7 +5,7 @@ import config from '../config';
 const { NODE_ENV = 'pro' } = config;
 const hostObj = {
   pro: 'http://172.16.225.8:8090',
-  dev: 'http://172.16.117.64:8090',
+  dev: 'http://172.16.117.65:8090',
   // dev: 'http://172.16.56.186:8090',
 };
 const HOST = hostObj[NODE_ENV];
