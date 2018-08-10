493de728051c30476ba7820297adacc8397df5bd
wangjing <1915093132@qq.com>
2018-08-10 20:58:04 +0800

    退费id列宽度调整

--- admin_sys/src/routes/Refund/RefundList.js
+++ admin_sys/src/routes/Refund/RefundList.js
@@ -148,7 +148,7 @@ class RefundList extends Component {
       {
         title: 'id',
         dataIndex: 'id',
-        width: '80px',
+        width: '90px',
       },
       {
         title: '子订单编号',
