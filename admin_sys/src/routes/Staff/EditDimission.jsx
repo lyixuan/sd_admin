18376bba3c869d92fa51d9c143db7b2042f90b55
lixioajing <lixiaojing01@sunlands.com>
2018-09-05 19:50:46 +0800

    编辑离职

--- admin_sys/src/routes/Staff/EditDimission.jsx
+++ admin_sys/src/routes/Staff/EditDimission.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
-import { Form, Button, DatePicker } from 'antd';
+import { Form, Button, DatePicker, Radio } from 'antd';
 import { assignUrlParams } from 'utils/utils';
-import { BaseUtils } from './BaseUtils';
+import { BaseUtils } from './BaseUtils.js';
 import ContentLayout from '../../layouts/ContentLayout';
 import common from '../Common/common.css';
 import styles from './styles/index.less';
 
 const dateFormat = 'YYYY-MM-DD';
+const RadioGroup = Radio.Group;
+
 @connect(({ staff, user, loading }) => ({
   staff,
   user,
   loading: loading.effects['staff/getStaffDetail'],
-  creatLoading: loading.effects['staff/addDemissionPost'],
+  creatLoading: loading.effects['staff/editDemissionPost'],
 }))
-class CreateDimission extends Component {
+class EditDimission extends Component {
   constructor(props) {
     const { urlParams = {} } = props;
     super(props);
@@ -30,6 +32,7 @@ class CreateDimission extends Component {
           effectDate: '',
         },
       ],
+      canceled: 1, // 是否撤销此次操作,1否,0是
     };
     this.state = assignUrlParams(initState, urlParams);
     this.baseUtils = new BaseUtils();
@@ -51,26 +54,32 @@ class CreateDimission extends Component {
     const { validateFields } = this.props.form;
     const { urlParams = {} } = this.props;
     validateFields((err, values) => {
-      const { effectDate } = values;
+      const { effectDate, canceled } = values;
       const paramsObj = Object.assign({}, this.state.kpiUserPositionLogList[0], {
         effectDate: effectDate.format(dateFormat),
+        canceled,
       });
       const newObj = {
         id: Number(urlParams.id),
         kpiUserPositionLogList: [paramsObj],
       };
+      console.log(newObj);
       this.props.dispatch({
-        type: 'staff/addDemissionPost',
+        type: 'staff/editDemissionPost',
         payload: newObj,
       });
     });
   };
-
+  ChangeAudio = e => {
+    const canceled = e.target.value;
+    this.setState({ canceled });
+  };
   backToList = () => {
     this.props.history.goBack();
   };
   render() {
     const { staff = {}, creatLoading } = this.props;
+    const { canceled } = this.state;
     const employeeInfo = staff.employeeInfo || {};
     const { FormItem } = this.baseUtils;
     const { getFieldDecorator } = this.props.form;
@@ -97,6 +106,21 @@ class CreateDimission extends Component {
               <li>
                 <span className={styles.labelText}>现任负责单位:</span>
                 <span className={styles.labelItem}>{employeeInfo.showName}</span>
+              </li>
+              <li className={styles.marB_24}>
+                <span className={styles.labelText}>撤销此次操作:</span>
+                <span className={styles.labelItem}>
+                  <FormItem>
+                    {getFieldDecorator('canceled', {
+                      initialValue: canceled,
+                    })(
+                      <RadioGroup onChange={this.ChangeAudio}>
+                        <Radio value={0}>是</Radio>
+                        <Radio value={1}>否</Radio>
+                      </RadioGroup>
+                    )}
+                  </FormItem>
+                </span>
               </li>
               <li className={styles.marB_24}>
                 <span className={styles.labelText}>*最后工作日期:</span>
@@ -134,5 +158,5 @@ class CreateDimission extends Component {
     );
   }
 }
-const WrappedDynamicRule = Form.create()(CreateDimission);
+const WrappedDynamicRule = Form.create()(EditDimission);
 export default WrappedDynamicRule;
