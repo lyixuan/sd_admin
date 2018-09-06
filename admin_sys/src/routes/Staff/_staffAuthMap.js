089fcc1a027e89a21a255ca8d549758c9e1316b4
mzh <manzhenhua@sunlands.com>
2018-09-06 16:50:07 +0800

    员工管理模块

--- admin_sys/src/routes/Staff/_staffAuthMap.js
+++ admin_sys/src/routes/Staff/_staffAuthMap.js
@@ -1,35 +1,39 @@
 const authMap = {
-  在岗: [{ title: '查看详情', path: '' }, { title: '转岗' }, { title: '休假' }, { title: '离职' }],
-  待转岗: [{ title: '查看详情' }, { title: '编辑转岗' }, { title: '休假' }, { title: '离职' }],
-  待休假: [{ title: '查看详情' }, { title: '编辑休假' }, { title: '转岗' }, { title: '离职' }],
-  休假中: [{ title: '查看详情' }, { title: '编辑休假' }, { title: '转岗' }, { title: '离职' }],
-  待离职: [{ title: '查看详情' }, { title: '编辑离职' }, { title: '转岗' }, { title: '休假' }],
-  已离职: [{ title: '查看详情' }],
+  在岗: { hasAuthName: ['查看详情', '转岗', '休假', '离职'], removeAuthName: ['编辑转岗'] },
+  待转岗: { hasAuthName: ['查看详情', '编辑转岗', '休假', '离职'], removeAuthName: ['转岗'] },
+  待离职: { hasAuthName: ['查看详情', '编辑离职', '转岗', '休假'], removeAuthName: ['离职'] },
+  已离职: { hasAuthName: ['查看详情'], removeAuthName: [] },
+  待休假: { hasAuthName: ['查看详情', '编辑休假', '转岗', '离职'], removeAuthName: ['休假'] },
+  休假中: { hasAuthName: ['查看详情', '编辑休假', '转岗', '离职'], removeAuthName: ['休假'] },
 };
 const buttonPath = {
   查看详情: '/privilege/staff/detail',
   转岗: '/privilege/staff/createTransJob',
-  休假: '/privilege/Staff/createHoliday',
-  离职: '/privilege/Staff/createDimission',
-  编辑转岗: '/privilege/Staff/editTransJob',
-  编辑休假: '/privilege/Staff/editHoliday',
-  编辑离职: '/privilege/Staff/editDimission',
+  休假: '/privilege/staff/createHoliday',
+  离职: '/privilege/staff/createDimission',
+  编辑转岗: '/privilege/staff/editTransJob',
+  编辑休假: '/privilege/staff/editHoliday',
+  编辑离职: '/privilege/staff/editDimission',
 };
 export function renderAuthButtonList(currentStateName = '') {
   let buttonList = [];
+  let removeAuthNameList = [];
   if (typeof currentStateName === 'string' && currentStateName.length > 0) {
     const nameList = currentStateName.split('、');
     Object.keys(authMap).forEach(item => {
       const isSame = nameList.find(name => name === item);
       if (isSame) {
-        buttonList = [...buttonList, ...authMap[item]];
+        buttonList = buttonList.concat(authMap[item].hasAuthName);
+        buttonList = [...new Set(buttonList)];
+        removeAuthNameList = removeAuthNameList.concat(authMap[item].removeAuthName);
+        removeAuthNameList = [...new Set(removeAuthNameList)];
       }
     });
   }
   const returnObj = [];
   buttonList.forEach(list => {
-    if (!returnObj.find(item => item.title === list.title)) {
-      returnObj.push({ ...list, path: buttonPath[list.title] });
+    if (!removeAuthNameList.find(item => item === list)) {
+      returnObj.push({ title: list, path: buttonPath[list] });
     }
   });
   return returnObj;
