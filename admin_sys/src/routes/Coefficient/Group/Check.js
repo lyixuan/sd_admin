529532f2d0543c0b5dedf400936970d5c4d8210b
wangjing <1915093132@qq.com>
2018-11-01 15:54:00 +0800

    添加小组绩效系数详情布局

--- admin_sys/src/routes/Coefficient/Group/Check.js
+++ admin_sys/src/routes/Coefficient/Group/Check.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import {  Button } from 'antd';
+import { assignUrlParams } from 'utils/utils';
+import ContentLayout from '../../../layouts/ContentLayout';
+import AuthorizedButton from '../../../selfComponent/AuthorizedButton';
+import common from '../../Common/common.css';
 
-export default class Check extends React.Component {
+
+@connect(({ account, loading }) => ({
+  account,
+  loading: loading.effects['account/accountList'],
+}))
+class Check extends Component {
+  constructor(props) {
+    super(props);
+    const params = this.props.getUrlParams();
+    const initParams = {
+      params: {
+        id:1,
+      },
+    };
+    this.state = assignUrlParams(initParams, params);
+
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+  getData = () => {
+    // const stateParams = this.state.params;
+    // const userListParams = { ...stateParams, ...params };
+    // this.props.dispatch({
+    //   type: 'user/userList',
+    //   payload:{userListParams},
+    // });
+  };
+
+  // 编辑
+  cancel = () => {
+    this.props.setRouteUrlParams('/performance/groupCoefficient/list', {});
+  };
+
   render() {
-    return <div>查看</div>;
+    // const { loading } = this.props;
+    return (
+      <ContentLayout
+        routerData={this.props.routerData}
+        contentButton={
+          <div>
+            <span className={common.titleWord}>生效周期 ：2018.10 ～ 至今</span>
+            <div className={common.rangeContent}>
+              <div >
+                <div className={common.rangeItemContent}>占位1</div>
+                <div className={common.xSpin} />
+                <div className={common.rangeItemContent}>占位2</div>
+                <div className={common.xSpin} />
+                <div className={common.rangeItemContent}>占位3</div>
+              </div>
+            </div>
+            <div style={{textAlign:'right'}}>
+              <AuthorizedButton authority="/account/createAccount">
+                <Button onClick={this.cancel} type="primary" className={common.createButton}>
+                  返回
+                </Button>
+              </AuthorizedButton>
+            </div>
+          </div>
+        }
+      />
+    );
   }
 }
+export default Check;
+
