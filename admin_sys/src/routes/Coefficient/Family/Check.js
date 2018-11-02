f6f0ff6b33bedd73a3ba5a9f33ba466e85783067
wangjing <1915093132@qq.com>
2018-11-02 17:53:44 +0800

    查看绩效管理系数详情页面代码提交

--- admin_sys/src/routes/Coefficient/Family/Check.js
+++ admin_sys/src/routes/Coefficient/Family/Check.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
-import { Button, Checkbox } from 'antd';
+import { Button } from 'antd';
 import { assignUrlParams } from 'utils/utils';
 import ContentLayout from '../../../layouts/ContentLayout';
 import AuthorizedButton from '../../../selfComponent/AuthorizedButton';
+import CoefficientDetail from '../../../selfComponent/Coefficient/CoefficientDetail';
 import common from '../../Common/common.css';
 
 @connect(({ account, loading }) => ({
@@ -41,6 +42,16 @@ class Check extends Component {
 
   render() {
     // const { loading } = this.props;
+    const data1={data:[{v1:0,v2:false,v3:0.2,v4:true,v5:8000},{v1:0.2,v2:false,v3:0.4,v4:false,v5:8000},{v1:0.4,v2:true,v3:1,v4:false,v5:8000}],
+      key:1,percent:1,basic:1}
+    const data2={data:[{v1:0,v2:false,v3:0.2,v4:true,v5:8000},{v1:0.2,v2:false,v3:0.4,v4:false,v5:8000},{v1:0.4,v2:true,v3:1,v4:false,v5:8000}],
+      key:1,percent:1,basic:1}
+    const data3={data:[{v1:0,v2:false,v3:0.2,v4:true,v5:8},{v1:0.2,v2:false,v3:0.4,v4:false,v5:8},{v1:0.4,v2:true,v3:1,v4:false,v5:8}],
+      key:1,percent:1,basic:2}
+    const data4={data:[{v1:0,v2:false,v3:0.2,v4:true,v5:8},{v1:0.2,v2:false,v3:0.4,v4:false,v5:8},{v1:0.4,v2:true,v3:1,v4:false,v5:8}],
+      key:1,percent:1,basic:3}
+    const data5={data:[{v1:1,v2:false,v3:20,v4:true,v5:2},{v1:20,v2:false,v3:40,v4:false,v5:1.5},{v1:40,v2:true,v3:100,v4:false,v5:2}],
+      key:1,percent:2,basic:3}
     return (
       <ContentLayout
         routerData={this.props.routerData}
@@ -51,31 +62,29 @@ class Check extends Component {
               <div >
                 <div className={common.rangeItemContent}>
                   <span className={common.titleWord}>人均在服学员排名比 (自考)</span>
+                  <CoefficientDetail dataSource={data1}  />
                 </div>
                 <div className={common.xSpin} />
                 <div className={common.rangeItemContent}>
                   <span className={common.titleWord}>人均在服学员排名比 (壁垒)</span>
-
-                  <Checkbox disabled checked className={common.checkBox}>
-                    闭区间
-                  </Checkbox>
+                  <CoefficientDetail dataSource={data2} />
                 </div>
                 <div className={common.xSpin} />
                 <div className={common.rangeItemContent}>
                   <span className={common.titleWord}>日均学分排名比 (自考)</span>
-                  <Checkbox  disabled className={common.checkBox}>
-
-                    闭区间
-                  </Checkbox>
+                  <CoefficientDetail dataSource={data3} />
                 </div>
                 <div className={common.xSpin} />
                 <div className={common.rangeItemContent}>
                   <span className={common.titleWord}>日均学分排名比 (壁垒)</span>
+                  <CoefficientDetail dataSource={data4}  />
                 </div>
                 <div className={common.xSpin} />
                 <div className={common.rangeItemContent}>
                   <span className={common.titleWord}>管理规模</span>
+                  <CoefficientDetail dataSource={data5} />
                 </div>
+                <div style={{height:'30px'}} />
               </div>
             </div>
             <div style={{ textAlign: 'right' }}>
---
+++ admin_sys/src/selfComponent/Coefficient/IntervalItem.js
@@ -0,0 +1,44 @@
+/* IntervalItem组件参数介绍:
+*
+* dataSource{必传 Obj}:数据源，对象结构:{v1:xx,v2:xx，percent:1/2}
+* v1:为数据
+* v：true/false:为checkbox的checked值为false还是true回显用的
+* percent:1是带%数据v1需要乘以100，2是不带%
+*
+* */
+
+import React, { Component } from 'react';
+import { Checkbox } from 'antd';
+// import styles from './CoefficientDetail.less';
+
+class IntervalItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+
+  coefficient = (v1, v2,percent) => {
+    console.log(v1, v2,percent)
+    const val1 =percent === 1?v1*100:v1
+    const bol = true
+    return (
+      <span>
+        <span>{val1}{percent===1?'%':null}</span>
+        <Checkbox  disabled checked={v2?bol:false}>
+         闭区间
+        </Checkbox>
+      </span>);
+  };
+
+  render() {
+    const {v1=0,v2=true,percent=1}=this.props;
+    return (
+      <span>
+        {this.coefficient(v1,v2,percent)}
+      </span>);
+  }
+}
+
+export default IntervalItem;
