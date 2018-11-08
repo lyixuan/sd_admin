c53d90d0c5169ab1dfc7fd7db661979530bb9ad7
lixioajing <lixiaojing01@sunlands.com>
2018-11-08 16:13:23 +0800

    浮点问题

--- admin_sys/src/routes/Coefficient/component/showPercentFn.js
+++ admin_sys/src/routes/Coefficient/component/showPercentFn.js
@@ -3,7 +3,6 @@
  * flag：*是乘法，/是除法
  * data：需要格式化的数据源
  */
-
 export function showPercentFn(data, type, flag) {
   Object.keys(data).map(item => {
     const res = data[item];
@@ -18,11 +17,11 @@ export function showPercentFn(data, type, flag) {
             res[i].groupKpi = value.groupKpi / 100;
           }
         } else {
-          res[i].levelLowerLimit = value.levelLowerLimit * 100;
-          res[i].levelUpperLimit = value.levelUpperLimit * 100;
+          res[i].levelLowerLimit = parseFloat((value.levelLowerLimit * 100).toPrecision(12));
+          res[i].levelUpperLimit = parseFloat((value.levelUpperLimit * 100).toPrecision(12));
           if (type === 3) {
-            res[i].classKpi = value.classKpi * 100;
-            res[i].groupKpi = value.groupKpi * 100;
+            res[i].classKpi = parseFloat((value.classKpi * 100).toPrecision(12));
+            res[i].groupKpi = parseFloat((value.groupKpi * 100).toPrecision(12));
           }
         }
       }
