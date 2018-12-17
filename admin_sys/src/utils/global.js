e2334ef21ac9a135f09665fafb75dd1af0865d1b
lyx <123>
2018-12-17 16:20:42 +0800

    修改filter方法

--- admin_sys/src/utils/global.js
+++ admin_sys/src/utils/global.js
@@ -2,30 +2,56 @@
 
 import constants from './constants';
 
-window.Filter = ({ constName = '', id = '', name = '' }) => {
+window.Filter = param => {
   /**
    * 获取静态常量(constants.js)中的数据
    *
-   * 1、根据常量constName获取常量数据
-   * 2、根据id获取name(常量需为Array)
-   * 3、根据name获取id(常量需为Array)
+   * @param String                    必传
+   * eg: "constName"                  1、根据常量constName获取常量数据
+   * eg: "constName[|id:yourId]"      2、根据id获取name(constName为Array)
+   * eg: "constName[|name:yourName]"  3、根据name获取id(constName为Array)
    *
-   * @param Object     eg:{constName='', id='', name=''}  说明：constName:必传 id:选传 name:选传; constName必须是constants.js文件中定义好的常量
+   * 说明: 中括号表示可选参数；constName必须是constants.js文件中定义好的常量
    * @return undefined or other
    * */
-  let result = constants[constName];
-  if (result && Array.isArray(result) && (id || name)) {
-    result = '';
-    // 查询常量名称存在，且常量值为数组，且有id或name参数
-    for (let i = 0; i < result.length; i += 1) {
-      if (result[i].id === String(id)) {
-        result = result[i].name;
-        break;
+  if (!param) {
+    console.warn('请传入字符串参数');
+    return false;
+  }
+
+  if (typeof param !== 'string') {
+    console.warn('参数类型应为string');
+    return false;
+  }
+
+  const tempArr = param.split('|');
+  const constName = tempArr[0];
+
+  // 第一个参数结果
+  let result = constants[constName] || null;
+
+  // 第二个参数结果
+  if (tempArr[1]) {
+    const type = tempArr[1].split(':')[0];
+    const value = tempArr[1].split(':')[1];
+    if (result && Array.isArray(result) && type && value) {
+      // 存在第二个参数，查询常量名称存在，且常量值为数组，且有id或name参数
+
+      const list = [...result];
+      result = null;
+      for (let i = 0; i < list.length; i += 1) {
+        if (type === 'id' && list[i].id === value) {
+          result = list[i].name;
+          break;
+        }
+        if (type === 'name' && list[i].name === value) {
+          result = list[i].id;
+          break;
+        }
       }
-      if (result[i].name === String(name)) {
-        result = result[i].id;
-        break;
-      }
+    } else {
+      console.warn('参数格式错误');
+      return false;
     }
   }
   return result;
