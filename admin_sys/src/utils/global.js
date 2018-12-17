9a8c3b4b7838218013b12992eece60d1c1c79f1b
lyx <123>
2018-12-17 14:57:18 +0800

    修改方法

--- admin_sys/src/utils/global.js
+++ admin_sys/src/utils/global.js
@@ -2,27 +2,27 @@
 
 import constants from './constants';
 
-window.Filter = (constName, item) => {
+window.Filter = ({ constName = '', id = '', name = '' }) => {
   /**
    * 获取静态常量(constants.js)中的数据
    *
-   * 1、根据常量Name获取常量数据(Array or other)
+   * 1、根据常量constName获取常量数据
    * 2、根据id获取name(常量需为Array)
    * 3、根据name获取id(常量需为Array)
    *
-   * @param String            constName      必传  说明：constName必须是constants.js文件中定义好的常量
-   * @param String or Number  item(id或name) 选传  说明：1.传id，根据id返回name；2.传name，根据name返回id 3.不传，返回constName的值
+   * @param Object     eg:{constName='', id='', name=''}  说明：constName:必传 id:选传 name:选传; constName必须是constants.js文件中定义好的常量
    * @return undefined or other
    * */
   let result = constants[constName];
-  if (result && Array.isArray(result) && item) {
-    // 查询常量名称存在，且常量值为数组，且有第二个参数
+  if (result && Array.isArray(result) && (id || name)) {
+    result = '';
+    // 查询常量名称存在，且常量值为数组，且有id或name参数
     for (let i = 0; i < result.length; i += 1) {
-      if (result[i].id === item) {
+      if (result[i].id === String(id)) {
         result = result[i].name;
         break;
       }
-      if (result[i].name === item) {
+      if (result[i].name === String(name)) {
         result = result[i].id;
         break;
       }
