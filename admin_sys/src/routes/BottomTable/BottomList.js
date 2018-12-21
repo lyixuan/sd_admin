1041dfb9c6d4aafac74c48e17f2bdadfc1591ef0
lixioajing <lixiaojing01@sunlands.com>
2018-12-21 10:12:41 +0800

    代码优化

--- admin_sys/src/routes/BottomTable/BottomList.js
+++ admin_sys/src/routes/BottomTable/BottomList.js
@@ -82,8 +82,8 @@ class BottomList extends Component {
 
   // 列表数据
   getDataList = paramObj => {
-    const { type, bottomTime, pageNum, pageSize } = this.state;
-    const params = { userId: 1187, type, bottomTime, pageNum, pageSize, ...paramObj };
+    const { userId, type, bottomTime, pageNum, pageSize } = this.state;
+    const params = { userId, type, bottomTime, pageNum, pageSize, ...paramObj };
     this.props.dispatch({
       type: 'bottomTable/bottomTableList',
       payload: params,
@@ -123,16 +123,18 @@ class BottomList extends Component {
 
   // 模态框确定
   clickModalOK = () => {
-    const { modalParam, userId } = this.state;
-    const { bottomDate, type } = modalParam;
-    if (bottomDate === '' || type === null) {
+    const { modalParam, type, userId, bottomTime, pageNum, pageSize } = this.state;
+    if (modalParam.bottomDate === '' || modalParam.type === null) {
       message.error('请完善所有信息');
       return;
     }
-
+    const sendParam = {
+      addParams: { ...modalParam, userId },
+      listParams: { userId, type, bottomTime, pageNum, pageSize },
+    };
     this.props.dispatch({
       type: 'bottomTable/addTask',
-      payload: { ...modalParam, userId },
+      payload: sendParam,
     });
     this.showModal(false);
   };
