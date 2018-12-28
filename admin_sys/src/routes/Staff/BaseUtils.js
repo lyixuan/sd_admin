5520d149fb2aeab97b1c5e8d7f0a446a2d62ef7f
mzh <manzhenhua@sunlands.com>
2018-12-28 15:20:15 +0800

    修改全局变量

--- admin_sys/src/routes/Staff/BaseUtils.js
+++ admin_sys/src/routes/Staff/BaseUtils.js
@@ -1,12 +1,11 @@
-/* eslint-disable no-undef */
-const FRONT_ROLE_TYPE_LIST = BI_Filter('FRONT_ROLE_TYPE_LIST');
+const FRONT_ROLE_TYPE_LIST = window.BI_Filter('FRONT_ROLE_TYPE_LIST');
 export default {
   groupTypeObj: FRONT_ROLE_TYPE_LIST.filter(item => item.id !== 'admin'),
   returnGroupType: (groupType = '') => {
-    return BI_Filter(`FRONT_ROLE_TYPE_LIST|id:${groupType}`).name;
+    return window.BI_Filter(`FRONT_ROLE_TYPE_LIST|id:${groupType}`).name;
   },
   returnOrganization: (groupType = '') => {
-    return BI_Filter(`FRONT_ROLE_TYPE_LIST|id:${groupType}`).name;
+    return window.BI_Filter(`FRONT_ROLE_TYPE_LIST|id:${groupType}`).name;
   },
   removeMailSymbal: (mail = '') => {
     const newMail = mail || '';
