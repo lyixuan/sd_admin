0b4ae904debbecd1a4a0379a94ec0b7d39a076e6
lyx <123>
2019-01-19 14:31:19 +0800

    审核 横线问题

--- src/routes/SkillCertification/AuditApply.js
+++ src/routes/SkillCertification/AuditApply.js
@@ -127,7 +127,7 @@ class AuditApply extends Component {
             </span>
           </div>
         ) : null}
-        {this.signExamineList && this.signExamineList.length > 1 ? <Divider /> : null}
+        {this.signExamineList && this.signExamineList.length !== i + 1 ? <Divider /> : null}
       </div>
     ));
     return (
