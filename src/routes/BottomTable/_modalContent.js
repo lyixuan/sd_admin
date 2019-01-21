181119da0c8c967689f6882203e0bfc3a61e824d
wangjing <1915093132@qq.com>
2019-01-21 11:58:22 +0800

    tij

--- src/routes/BottomTable/_modalContent.js
+++ src/routes/BottomTable/_modalContent.js
@@ -1,6 +1,6 @@
 // 自定义弹框
 import React from 'react';
-import { Radio, Select, DatePicker } from 'antd';
+import { Select, DatePicker } from 'antd';
 import { DATA_ANALYST_ID } from '../../utils/constants';
 
 const dateFormat = 'YYYY-MM-DD';
@@ -16,10 +16,10 @@ export default class ModalContent extends React.Component {
     };
   }
   // 底表类型选择
-  onRadioChange = e => {
-    this.setState({ type: e.target.value });
-    this.props.updateModalData({ ...this.state, type: e.target.value });
-  };
+  // onRadioChange = e => {
+  //   this.setState({ type: e.target.value });
+  //   this.props.updateModalData({ ...this.state, type: e.target.value });
+  // };
   // 底表时间选择
   onDateChange = (date, dateString) => {
     this.setState({ bottomDate: dateString });
@@ -67,7 +67,18 @@ export default class ModalContent extends React.Component {
       <>
         <>
           <span>底表类型：</span>
-          <Radio.Group
+          <span
+            style={{
+              width: '230px',
+              display: 'inline-block',
+              textAlign: 'left',
+              color: 'rgba(0, 0, 0, 0.45);',
+            }}
+          >
+            学分底表{' '}
+          </span>
+          {/*
+            <Radio.Group
             onChange={this.onRadioChange}
             value={this.state.type}
             style={{ width: '230px' }}
@@ -79,6 +90,7 @@ export default class ModalContent extends React.Component {
               预估分底表
             </Radio>
           </Radio.Group>
+           */}
         </>
         <div style={{ margin: '17px auto' }}>
           <span>底表时间：</span>
