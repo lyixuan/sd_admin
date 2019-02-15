01bede39efbf08ce8ae9621496ece9382a9227a6
lixioajing <lixiaojing01@sunlands.com>
2019-02-15 14:04:57 +0800

    优化editor

--- src/routes/ExcellentCase/Components/wangEditor.js
+++ src/routes/ExcellentCase/Components/wangEditor.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import E from 'wangeditor';
 
-class App extends Component {
+class Editor extends Component {
   componentDidMount() {
     const elem = document.getElementById('editorElem');
     const editor = new E(elem);
@@ -10,11 +10,30 @@ class App extends Component {
         this.props.onChange(html);
       }
     };
+
+    if (editor.customConfig) {
+      editor.customConfig.menus = [
+        'bold', // 粗体
+        'fontSize', // 字号
+        'fontName', // 字体
+        'italic', // 斜体
+        'underline', // 下划线
+        'foreColor', // 文字颜色
+        'justify', // 对齐方式
+        'image', // 插入图片
+        'undo', // 撤销
+        'redo', // 重复
+      ];
+      editor.customConfig.uploadImgShowBase64 = true;
+      // editor.customConfig.uploadImgServer ='/server';
+      editor.customConfig.showLinkImg = false;
+    }
     editor.create();
+    editor.txt.html('<p>请输入...</p>');
   }
   render() {
     return <div id="editorElem" style={{ textAlign: 'left' }} />;
   }
 }
 
-export default App;
+export default Editor;
