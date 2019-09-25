c16e01bdabcb1bc2066e601b38cd550577268e6a
lyx <123>
2019-09-25 16:53:17 +0800

    创收4

--- src/routes/BottomOrder/CreateIncome/List.js
+++ src/routes/BottomOrder/CreateIncome/List.js
@@ -293,7 +293,7 @@ class CreateList extends Component {
   onSubmit = val => {
     const params = { ...val };
     if (!params.recommendedTeacher) {
-      message.warn('邮箱未查询到用户名');
+      message.warn('未查找到该老师信息');
       return;
     }
     params.competitionRatio = params.competitionRatio === undefined ? 100 : params.competitionRatio;
