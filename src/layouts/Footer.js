b1d9518e4e0c7506e963b75b2428a3325086b990
lyx <123>
2019-11-19 14:57:58 +0800

    动态footer内容

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import styles from './styles/footer.less';
+import { SERVER_HOST } from '../utils/constants';
+import request from '../utils/request';
+
+const HOST_NEW2 = `${SERVER_HOST}/deskperfpcapi`;
 
 export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      footerName: '',
+    };
+    const that = this;
+    request(`${HOST_NEW2}/globalInfo/getFooter`).then(resp => {
+      if (resp && resp.code === 20000) {
+        that.setState({
+          footerName: resp.data.value,
+        });
+      }
+    });
+  }
   render() {
-    return <div className={styles.footerLayout}>Powered by 后端运营纵线 / 产研部</div>;
+    return <div className={styles.footerLayout}>{this.state.footerName}</div>;
   }
 }
