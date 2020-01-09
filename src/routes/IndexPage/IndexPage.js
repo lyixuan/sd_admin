c6c3cf2f1fae5e647c692f79e2a562a10127dfb4
seven <756465934@qq.com>
2020-01-09 19:48:03 +0800

    修改首页图片

--- src/routes/IndexPage/IndexPage.js
+++ src/routes/IndexPage/IndexPage.js
@@ -3,6 +3,9 @@ import styles from './indexPage.less';
 import homeImg from '../../assets/homeImg.png';
 import homeText from '../../assets/homeText.png';
 import { INDEX_HOST } from '../../utils/constants';
+import topImage from '../../assets/indexPage/top-image.png';
+import centerImage from '../../assets/indexPage/center-image.png';
+import bottomImage from '../../assets/indexPage/bottom-image.png';
 
 class IndexPage extends Component {
   componentDidMount() {
@@ -15,10 +18,11 @@ class IndexPage extends Component {
     return (
       <div className={styles.container}>
         <div className={styles.content}>
-          <img src={homeImg} alt="首页" className={styles.homeImg} />
-          <div className={styles.userDescription}>
-            <img src={homeText} alt="首页文字" />
+          <img src={topImage} alt="图片" className={styles.top}/>
+          <div className={styles.center}>
+            <img src={centerImage} alt="图片" className={styles.centerImage}/>
           </div>
+          <img src={bottomImage} alt="图片" className={styles.bottom}/>
         </div>
       </div>
     );
